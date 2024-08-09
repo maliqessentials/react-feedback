38a33fa301e055d881424fd485f040b20b922951
malikkashraf <malikasyraf30@gmail.com>
2024-08-09 01:19:29 -0700

    Feedback
    selesai

--- rating-dashboard/src/resume.jsx
+++ rating-dashboard/src/resume.jsx
@@ -3,12 +3,15 @@
 import React from 'react';
 
 const Resume = () => {
-  return (
+  return (  
     <div className="p-8 max-w-4xl mx-auto">
-      <div className="border-b pb-4 mb-4">
-      <h3 className="font-serif text-xl font-semibold">PERSONAL INFORMATION</h3>
-        <hr className='border-b-black'></hr>
-        <p className="text-gray-600">Tempat Lahir: Sumut</p>
+      <div className="border-b pb-4 mb-4"> 
+      <h3 className="font-poppins text-xl font-semibold">PERSONAL INFORMATION</h3>
+        <hr className='border-black mb-4' /> 
+        <div className='flex mb-2'></div>
+        <span className='w-48 font-bold'>Tempat Lahir :</span>
+        <span className='text-gray-600'>Sumut</span>
+        <p className="text-gray-600">Tempat Lahir:Sumut</p>
         <p className="text-gray-600">Tanggal Lahir: 2024-07-04</p>
         <p className="text-gray-600">Email: refsimaulanasrg@gmail.com</p>
         <p className="text-gray-600">No. Handphone: 085267516715</p>
@@ -24,7 +27,7 @@ const Resume = () => {
           Skilled Front-end Developer with 4 years of experience in designing, developing and maintaining front-end web applications. Achieved measurable success in increasing page load speeds by 20% which lead to an increase in website conversion rates. Led the implementation of new front-end development system which resulted in a 15% reduction in development time and a 20% increase in website performance. Adept in collaborating with cross-functional teams and delivering projects on-time and on-budget.
         </p>
       </div>
-
+      
       <div className="mb-4">
         <h2 className="text-2xl font-bold">Pendidikan</h2>
         <p className="text-gray-600">Pendidikan Terakhir: S1</p>
